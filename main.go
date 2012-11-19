9a29ea4b7f08b44fd51b30abd5e64d29eb3ba196
Khaja Minhajuddin <minhajuddin@cosmicvent.com>
2012-11-19 22:42:49 +0530

    init

---
+++ main.go
@@ -0,0 +1,44 @@
+package mvc
+
+import (
+  "log"
+	"fmt"
+	"net/http"
+)
+
+type RouteHandler struct {
+	Version int
+}
+
+//dummy types
+type Result string
+type Context string
+
+type Action func(c Context) Result
+
+var routes = make(map[string]Action)
+
+func (handler *RouteHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+  path := r.URL.Path
+  if a, ok := routes[path]; ok {
+    log.Println("found action for", path)
+		fmt.Fprintln(w, a(Context("Some stuff")))
+		return
+	}
+  log.Println("no action found for", path)
+	http.NotFound(w, r)
+}
+
+func RouteMap(pattern string, handler Action){
+  routes[pattern] = handler
+}
+
+func StartServer(port string){
+  log.Printf("Starting server on http://localhost%s/", port)
+	handler := RouteHandler{Version: 1}
+	http.ListenAndServe(port, &handler)
+}
+
+//initialization code
+func init(){
+}
